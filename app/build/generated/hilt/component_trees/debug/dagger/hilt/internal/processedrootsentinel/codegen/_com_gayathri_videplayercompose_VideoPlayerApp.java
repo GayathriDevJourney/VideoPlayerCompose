071696a64e2af6f17742e760f7ce70a9e2d2c9f4
GayathriBangera <59442681+GayathriBangera@users.noreply.github.com>
2024-01-16 16:18:10 +0530

    Delete app/build directory

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_gayathri_videplayercompose_VideoPlayerApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.gayathri.videplayercompose.VideoPlayerApp"
-)
-public final class _com_gayathri_videplayercompose_VideoPlayerApp {
-}
